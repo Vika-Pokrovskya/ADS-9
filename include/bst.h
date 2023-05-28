f1a5180da477aeae6d018d419c1abc0dce95e0a6
Vika-Pokrovskya <117218299+Vika-Pokrovskya@users.noreply.github.com>
2023-05-28 21:04:22 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,37 +4,50 @@
 template<typename T>
 class BST {
  private:
-    struct Node {
-        T value;
-        int count;
-        Node* left, * right;
-    };
-    Node* root;
-    Node* addNode(Node* root, const T& value) {
-        if (root == nullptr) {
-            root = new Node;
-            root->value = value;
-            root->count = 1;
-            root->left = root->right = nullptr;
-        } else if (root->value > value) {
-            root->left = addNode(root->left, value);
-        } else if (root->value < value) {
-            root->right = addNode(root->right, value);
-        } else {
-            root->count += 1;
-        }
-        return root;
+  struct Node {
+    T value;
+    int count;
+    Node *l;
+    Node *r;
+  };
+  Node *root;
+  Node* addNode(Node *root, T v) {
+    if (!root) {
+      root = new Node;
+      root->value = v;
+      root->l = root->r = nullptr;
+    } else if (root->value > v) {
+      root->l = addNode(root->l, v);
+    } else if (root->value < v) {
+      root->r = addNode(root->r, v);
+    } else {
+      (root->count)++;
     }
-    Node* searchNode(Node* root, const T& value) {
-        if (root == nullptr || root->value == value) return root;
-        if (value < root->value) return searchNode(root->left, value);
-        return searchNode(root->right, value);
+    return root;
+  }
+  int searchValue(Node* root, T v) {
+    if (!root) {
+      return 0;
+    } else if (root->value == v) {
+      return root->count;
+    } else if (root->value > v) {
+      return searchValue(root->l, v);
+    } else {
+      return searchValue(root->r, v);
     }
-    int getDepth(Node* root) {
-        if (root == nullptr) return 0;
-        return std::max(getDepth(root->left), getDepth(root->right)) + 1;
+  }
+  int heightTree(Node* root) {
+    if (!root) {
+      return 0;
     }
-  
+    int L = heightTree(root->l);
+    int R = heightTree(root->r);
+    if (L > R) {
+      return (L + 1);
+    } else {
+      return (R + 1);
+    }
+  }
 
  public:
   BST() :root(nullptr) {}
