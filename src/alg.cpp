1227b36cb21b5b2a610555936b91100f4a0ec372
Vika-Pokrovskya <117218299+Vika-Pokrovskya@users.noreply.github.com>
2023-05-28 20:53:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,26 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+std::string lower(std::string s) {
+  for (auto& x : s) {
+    x = tolower(x);
+  }
+  return s;
+}
+
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> Tree;
+  std::ifstream fin(filename);
+  std::string w = "";
+  while (!fin.eof()) {
+    char c = fin.get();
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+      w += c;
+    } else {
+      w = lower(w);
+      Tree.add(w);
+      w = "";
+    }
+  }
+  return Tree;
 }
